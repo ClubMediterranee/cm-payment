a69a0fa486d740c91ef54d11a2df4fa62b8946a0
Romain Lenzotti <rom.lenzotti@gmail.com>
2025-09-30 13:56:54 +0200

    ci: update chromatic command to run in non-interactive mode and change vite config path extension

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -33,7 +33,7 @@ const config: StorybookConfig = {
     builder: {
       name: '@storybook/builder-vite',
       options: {
-        viteConfigPath: './vite.storybook.config.js',
+        viteConfigPath: './vite.storybook.config.ts',
       },
     },
     disableTelemetry: true,
