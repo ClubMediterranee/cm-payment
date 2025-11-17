ceb1eed018400d734b374cb07411c89f02b28fed
jbueno1 <jerome.bueno1@gmail.com>
2025-11-17 15:20:24 +0100

    fix: add error boundary

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css';
 
+import { ErrorBoundary } from 'react-error-boundary';
+
 import { RootProviders } from './providers/RootProvider';
 import { Router } from './Router';
 
 function App() {
   return (
-    <RootProviders>
-      <Router />
-    </RootProviders>
+    <ErrorBoundary fallback={<h1>Something went wrong</h1>}>
+      <RootProviders>
+        <Router />
+      </RootProviders>
+    </ErrorBoundary>
   );
 }
 
