6bd560d2ce45b19410c1666cafd64f6bd73dec4b
jbueno1 <jerome.bueno1@gmail.com>
2025-12-11 11:31:06 +0100

    fix: remove bnpl provider

--- packages/sdk/src/hooks/data/usePaymentProviders.ts
+++ packages/sdk/src/hooks/data/usePaymentProviders.ts
@@ -1,14 +1,32 @@
 import { PaymentConfig } from '@clubmed/payment-sdk/types/PaymentConfig';
 import { useSuspenseQuery } from '@tanstack/react-query';
 
-import { getV1PaymentProviders } from '../../__generated__';
+import { getV1PaymentProviders, PaymentProvider1CategoryPaymentMethod } from '../../__generated__';
 import { usePaymentConfig } from './usePaymentConfig';
 
 export const paymentProvidersQueryOptions = (providerConfig: PaymentConfig['providers']) => ({
   queryKey: ['paymentProviders'],
   queryFn: getV1PaymentProviders,
   select: (providers: Awaited<ReturnType<typeof getV1PaymentProviders>>) => {
-    return providers.filter((provider) => providerConfig[provider.id]?.is_active);
+    return providers
+      .filter((provider) => providerConfig[provider.id]?.is_active)
+      .reduce(
+        (acc, provider) => {
+          if (
+            provider.category_payment_method ===
+            PaymentProvider1CategoryPaymentMethod.BuyNowPayLater
+          ) {
+            acc.buyNowPayLaterProviders.push(provider);
+          } else {
+            acc.paymentProviders.push(provider);
+          }
+          return acc;
+        },
+        {
+          paymentProviders: [] as typeof providers,
+          buyNowPayLaterProviders: [] as typeof providers,
+        },
+      );
   },
 });
 
--- packages/sdk/src/hooks/utils/useWatchedPaymentProvider.ts
+++ packages/sdk/src/hooks/utils/useWatchedPaymentProvider.ts
@@ -2,8 +2,12 @@ import { usePaymentProviders } from '../data/usePaymentProviders';
 import { useWatch } from './useForm';
 
 export const useWatchedPaymentProvider = () => {
-  const { data: paymentProviders } = usePaymentProviders();
+  const {
+    data: { paymentProviders, buyNowPayLaterProviders },
+  } = usePaymentProviders();
   const watchedProviderId = useWatch('provider_id');
 
-  return paymentProviders?.find((provider) => provider.id === watchedProviderId);
+  return [...paymentProviders, ...buyNowPayLaterProviders]?.find(
+    (provider) => provider.id === watchedProviderId,
+  );
 };
